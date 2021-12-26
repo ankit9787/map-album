29c57b1db4135288805fa2fa153249026003fa1e
Ankit Singh <34910004+ankit9787@users.noreply.github.com>
2021-12-26 10:21:49 +0530

    modal from component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +11,18 @@ export class AppComponent {
   filePath = "";
   imageUrls: any;
 
+constructor(private modalService: NgbModal) {}
+
+
 getFilePath(value: String): void{
   this.filePath="assets/"+value+"/sample1.jpg";
   this.imageUrls = ["assets/"+value+"/sample1.jpg", "assets/"+value+"/sample2.jpg" ]
   console.log(this.filePath);
+}
+
+openModal(modalData: any, value: any) {
+  this.imageUrls = ["assets/"+value+"/sample1.jpg", "assets/"+value+"/sample2.jpg" ]
+  this.modalService.open(modalData);
 }
 
 }
