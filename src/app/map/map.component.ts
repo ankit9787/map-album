7f2fb384e95e1c23c91fab5a302cfdcd4828bac1
Ankit Singh <34910004+ankit9787@users.noreply.github.com>
2021-12-26 20:19:41 +0530

    show point and polygon on map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,20 @@
 import { AfterViewInit, Component, OnInit} from '@angular/core';
 import * as L from 'leaflet';
+import * as geojson from 'geojson';
+const iconRetinaUrl = './assets/marker-icon-2x.png';
+const iconUrl = './assets/marker-icon.png';
+const shadowUrl = './assets/marker-shadow.png';
+const iconDefault = L.icon({
+iconRetinaUrl,
+iconUrl,
+shadowUrl,
+iconSize: [25, 41],
+iconAnchor: [12, 41],
+popupAnchor: [1, -34],
+tooltipAnchor: [16, -28],
+shadowSize: [41, 41]
+});
+L.Marker.prototype.options.icon = iconDefault;
 
 @Component({
   selector: 'app-map',
@@ -8,6 +23,7 @@ import * as L from 'leaflet';
 })
 export class MapComponent implements AfterViewInit {
   private map: any;
+  abc = false;
 
   private initMap(): void {
     this.map = L.map('map', {
@@ -25,14 +41,10 @@ export class MapComponent implements AfterViewInit {
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     .openPopup();
 
-    var circle = L.circle([51.508, -0.11], {
-      color: 'red',
-      fillColor: '#f03',
-      fillOpacity: 0.5,
-      radius: 500
-  }).addTo(this.map);
 
-  this.map.on('click', this.onMapClick, this);
+    this.map.on('click', this.onMapClick, this);
+
+    
   }
 
   onMapClick(e: any) {
@@ -54,4 +66,80 @@ export class MapComponent implements AfterViewInit {
     .openPopup();
   }
 
+  createMarker(){
+    console.log("hello")
+    var geojsonFeature:any = [{
+      "type": "Feature",
+      "properties": {
+          "name": "Coors Field",
+          "amenity": "Baseball Stadium",
+          "popupContent": "This is where the Rockies play!"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [78, 22]
+      }
+  }];
+
+  L.geoJSON(geojsonFeature).addTo(this.map);
+
+var states:any = [{
+  "type": "Feature",
+  "properties": {"party": "Republican"},
+  "geometry": {
+      "type": "Polygon",
+      "coordinates": [[
+          [-104.05, 48.99],
+          [-97.22,  48.98],
+          [-96.58,  45.94],
+          [-104.03, 45.94],
+          [-104.05, 48.99]
+      ]]
+  }
+}, {
+  "type": "Feature",
+  "properties": {"party": "Democrat"},
+  "geometry": {
+      "type": "Polygon",
+      "coordinates": [[
+          [-109.05, 41.00],
+          [-102.06, 40.99],
+          [-102.03, 36.99],
+          [-109.04, 36.99],
+          [-109.05, 41.00]
+      ]]
+  }
+}];
+
+console.log(states);
+
+
+L.geoJSON(states).addTo(this.map);
+
+
+  }
+
 }
+
+    // var marker = L.marker([20.726875, 77.871094]).on('click', onClick);
+    // function onClick(e: any) {alert(e.latlng);}
+    // marker.addTo(this.map)
+
+
+//   L.geoJSON(geojsonFeature, {
+//     pointToLayer: function (feature, latlng) {
+//         return L.circleMarker(latlng, geojsonMarkerOptions);
+//     }
+// }).addTo(this.map);
+
+  // "type": "LineString",
+  //       "coordinates": [[78, 22], [79, 21], [79, 17]]
+
+//   var geojsonMarkerOptions = {
+//     radius: 8,
+//     fillColor: "#ff7800",
+//     color: "#000",
+//     weight: 1,
+//     opacity: 1,
+//     fillOpacity: 0.8
+// };
